9eb9c9e005acc1eeb89dac917bb99e047a0f2d90
codymalick <cody.malick@gmail.com>
2015-12-07 19:09:07 -0800

    Working

---
+++ cs344/4/otp_dec_d.c
@@ -0,0 +1,99 @@
+//From example file provided by teacher
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BUFF 50000
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     char buffer[BUFF];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+
+	char *key;
+	char *message;
+
+	while(1) {
+
+		clilen = sizeof(cli_addr);
+     		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+     		if (newsockfd < 0) 
+          		error("ERROR on accept");
+		int pid = fork();
+		if(pid == 0) {
+			bzero(buffer,BUFF);
+     			n = read(newsockfd,buffer,BUFF);
+     			if (n < 0) error("ERROR reading from socket");
+     			//printf("%s\n",buffer);
+
+			//do stuff
+			message = strtok(buffer, "~");	
+			key = strtok(NULL, "`");	
+
+			message = strtok(message, "\n");
+			key = strtok(key, "\n");
+			
+			//printf("%s\n", key); //debug
+			//printf("%s\n", message); //debug
+
+			//decrypt
+			
+			char set[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+			int strlength = strlen(message);
+			int j, n, m, t;
+			char cipher[BUFF];
+			for(j = 0; j < strlength; j++) {
+				n = message[j] - 65;
+				m = key[j] - 65;
+				if(n == -33)
+					n = 26;
+				if(m == -33)
+					m = 26;
+				t = (n-m) % 27;
+				if(t < 0)
+					t += 27;
+				cipher[j] = set[t];
+			}
+			cipher[j] = '\n';
+
+     			n = write(newsockfd, cipher, BUFF);
+     			if (n < 0) error("ERROR writing to socket");
+			break;
+		}
+     		else {
+			close(newsockfd);
+		}
+     		//close(sockfd);
+	}
+         return 0; 
+}
--- cs344/4/otp_enc_d.c
+++ cs344/4/otp_enc_d.c
@@ -1,79 +1,99 @@
-#include<stdlib.h>
-#include<string.h>
-#include<stdio.h>
-#include<sys/types.h>
-#include<errno.h>
-#include<sys/socket.h>
-#include<unistd.h>
-#include<netinet/in.h>
+//From example file provided by teacher
 
-#define USAGE "otp_enc_d [listening_port]"
-#define BUFF 4096
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-char key[4096];
+#define BUFF 50000
 
-char* encrypt(char* plain[BUFF]);
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
 
-//otp_enc_d [listening_port]
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     char buffer[BUFF];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
 
-	if(argc < 2 || !argv[1]) {
-		fprintf(stderr, "USAGE: %s\n", USAGE);
-		exit(1);
-	}
-	int port;
-	if(!(port = atoi(argv[1]))) {
-		fprintf(stderr, "Invalid Input: argv[1]");
-		exit(1);
-	}
-
-	//Temp for testing pipes
-	//if(argc < 3) {
-	//	scanf("%s", key);
-	//	printf("key %s\n", key);
-	//}
-		
-	struct sockaddr_in server;
-	int sock;
-	char message[BUFF];	
-
-	server.sin_family = AF_INET;
-	server.sin_port = htons(port);
-	server.sin_addr.s_addr = INADDR_ANY;
-
-	//Create the socket and set it up to accept connections
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-
-	bind(sock, (struct sockaddr *) &server, sizeof(server));
-	if(listen(sock, 1) < 0) {
-			perror("listen");
-			exit(1);
-	}
-	int pid;
-
-	for(int f = 0; f < 5; f++) {
-		pid = fork();
-			
-		if(pid == 0) {
-			break;
-		}
-		printf("%i\n", pid);
-	}
-
-
+	char *key;
+	char *message;
 
 	while(1) {
+
+		clilen = sizeof(cli_addr);
+     		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+     		if (newsockfd < 0) 
+          		error("ERROR on accept");
+		int pid = fork();
 		if(pid == 0) {
-			int clientsock = accept(sock, NULL, NULL);
-			ssize_t r;
-			while((r = recv(clientsock, message, sizeof(message), 0)));
-			printf("%s\n", message);
-			//enc/dec stuff	
-		} else {
-			//parent
-			wait();
-			pid = fork();
+			bzero(buffer,BUFF);
+     			n = read(newsockfd,buffer,BUFF);
+     			if (n < 0) error("ERROR reading from socket");
+     			//printf("%s\n",buffer);
+
+			//do stuff
+			message = strtok(buffer, "~");	
+			key = strtok(NULL, "`");	
+
+			message = strtok(message, "\n");
+			key = strtok(key, "\n");
+			
+			//printf("%s\n", key); //debug
+			//printf("%s\n", message); //debug
+
+			//encrypt
+			
+			char set[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+			int strlength = strlen(message);
+			int j, n, m, t;
+			char cipher[BUFF];
+			for(j = 0; j < strlength; j++) {
+				n = message[j] - 65;
+				m = key[j] - 65;
+				if(n == -33)
+					n = 26;
+				if(m == -33)
+					m = 26;
+				t = (n+m) % 27;
+				if(t < 0)
+					t += 27;
+				cipher[j] = set[t];
+			}
+
+			cipher[j] = '\n';
+     			n = write(newsockfd, cipher, BUFF);
+     			if (n < 0) error("ERROR writing to socket");
+			break;
 		}
-	}	
-	return 0;
+     		else {
+			close(newsockfd);
+		}
+     		//close(sockfd);
+	}
+         return 0; 
 }
