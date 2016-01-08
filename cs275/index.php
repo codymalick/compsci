1524407ebe39c24d2dcc2211dec261e00ecbda24
Cody Malick <cody.malick@gmail.com>
2016-01-07 16:25:54 -0800

    Found cs275, it's glorious

---
+++ cs275/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="style.css">
+	<TITLE>Home</TITLE>
+	<div id="banner">
+		<h1>Student Information Center</h1>
+	</div>
+	<br>
+	<body>
+		<div id="side">
+			<ul>
+				<li>
+					<a href="index.php">Home</a>
+				</li>
+				<br>
+				<li>
+					<a href="student_information.html">Submit a Record</a>
+				</li>
+				<br>
+				<li>
+					<a href="student_list.php">Student List</a>
+				</li>
+				<br>
+
+			</ul>
+		</div>
+		<div id="main">
+			Student Information Page
+			<br>
+			<a href="student_information.html">Click here to submit your student information!</a>
+		</div>
+		
+	</body>
+</html>
