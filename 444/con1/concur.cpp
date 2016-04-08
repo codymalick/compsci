d2a080b49df100ab11891314a5d1b91173a230fe
codymalick <cody.malick@gmail.com>
2016-04-08 16:09:31 -0700

    fixed some styling

--- 444/con1/concur.cpp
+++ 444/con1/concur.cpp
@@ -2,10 +2,10 @@
 #include<time.h>
 #include<stdlib.h>
 #include<unistd.h>
-//pthread.h include for pthreads (incase it wasn't obvious)
+/*pthread.h include for pthreads (incase it wasn't obvious)*/
 #include<pthread.h>
 
-//mutex declaration
+/*mutex declaration*/
 pthread_mutex_t mutex;
 
 struct message {
@@ -18,44 +18,45 @@ struct buffer {
     int size;  
 };
 
-void buff_push(struct buffer *buff, struct message m) {
+void buff_push(struct buffer *buff, struct message m)
+{
     buff->b[buff->size] = m;
     buff->size++;
 }
 
-void buff_pop(struct buffer *buff) {
-    //shift all items towards the front one space
+void buff_pop(struct buffer *buff)
+{
+    /*shift all items towards the front one space*/
     for(int i = 1; i < buff->size; i++) {
         buff->b[i-1] = buff->b[i];
     }
     buff->size--;
 }
 
-int rand_gen() {
+int rand_gen()
+{
     srand(time(NULL));
     return rand() % 10;
 }
 
-void *factory_function(void *ptr) {
-    //cast void pointer to buffer pointer
+void *factory_function(void *ptr)
+{
+    /*cast void pointer to buffer pointer*/
     struct buffer *buff = (buffer *)ptr;
     struct message item;
 
     while(1) {
         
-        //check if buffer is full
+        /*check if buffer is full*/
         if(buff->size < (sizeof(buff->b)/sizeof(struct message))) {
             
-            //lock resources
             pthread_mutex_lock(&mutex); 
             
-            //initialize new message
             item.number = rand_gen();
             item.wait_period = rand_gen();
            
             buff_push(buff, item); 
             
-            //unlock resources
             pthread_mutex_unlock(&mutex);
             
             printf("%i:%i:%i:%i\n",item.number, item.wait_period, buff->size,
@@ -64,9 +65,9 @@ void *factory_function(void *ptr) {
     }
 }
 
-void *consumer_function(void *ptr) {
-    
-    //cast void pointer to buffer pointer
+void *consumer_function(void *ptr)
+{   
+    /*cast void pointer to buffer pointer*/
     struct buffer *buff = (buffer*)ptr;
     struct message item;
     int wait_time = 0;
@@ -74,63 +75,65 @@ void *consumer_function(void *ptr) {
 
         if(buff->size > 0) {
             
-            //lock resources
             pthread_mutex_lock(&mutex);
 
             item = buff->b[0];
             buff_pop(buff);
                      
-            //unlock resources
             pthread_mutex_unlock(&mutex);
 
             sleep(item.wait_period);
             printf("Number: %i, Wait: %i\n", item.number, item.wait_period);
         }
     }
-
-    printf("%s\n", "print 2");
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    /*thread declaration*/
+    pthread_t factory;
+    pthread_t consumer;
 
-    //thread declaration
-    pthread_t factory, consumer;
-
-    //buffer struct init
+    /*buffer struct init*/
     struct buffer *buff;
     buff = (struct buffer *)malloc(sizeof(struct buffer));
     buff->size = 0;
     
-    //variables for error checking
-    int t_1, t_2;
-
-    //mutex init 
+    /*mutex init*/ 
     pthread_mutex_init(&mutex, NULL);
-    
-    //Create new thread
-    //pthread_create(pthread_t *thread, const pthread_attr_t *attr,
-    //  void *(*start_routine) (void *), void *arg);
+ 
+    /*variables for error checking*/
+    int t_1, t_2;
+       
+    /*
+     *Create new thread
+     *Pthread_create doc:
+     *pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+     *  void *(*start_routine) (void *), void *arg);
+     */
     t_1 = pthread_create(&factory, NULL, factory_function, (void*)buff);
+    
     if(t_1) {
         fprintf(stderr, "Error, pthread_create() - exit: %i\n", t_1);
         exit(t_1);
     }
 
     t_2 = pthread_create(&consumer, NULL, consumer_function, (void*)buff);
+    
     if(t_2) {
         fprintf(stderr, "Error, pthread_create() - exit: %i\n", t_2);
         exit(t_2);
     }
 
+    /*
+     * This code is never run due to infinite loops, but if the program does
+     * terminate, all memory is freed and threads complete properly.
+     */
 
-    //printf("pthread_create for thread 1 returns: %i\n", t_1);
-    //printf("pthread_create for thread 2 returns: %i\n", t_2);
-    
-    //wait for threads to complete, barrier
+    /*wait for threads to complete, barrier*/
     pthread_join(factory, NULL);
     pthread_join(consumer, NULL);
 
-    //free buffer struct
     free(buff);
     exit(0);
 }
