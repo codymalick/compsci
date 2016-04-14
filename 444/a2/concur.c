db556d9b4fcb60dc3af2e4804b4c7c747a11c69f
codymalick <cody.malick@gmail.com>
2016-04-14 08:50:26 -0700

    cleanup con1, added a2 base files

---
+++ 444/a2/concur.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h> /*pthread.h include for pthreads*/
+
+/*mutex declaration*/
+pthread_mutex_t mutex;
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
