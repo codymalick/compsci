3809b6d1bc080bf6aacea632397c736af2913f09
codymalick <cody.malick@gmail.com>
2016-05-02 17:36:42 -0700

    concur3 done

--- 444/con3/concur.c
+++ 444/con3/concur.c
@@ -11,13 +11,11 @@ pthread_mutex_t delete_mutex;
 
 struct node {
 	int data;
-	pthread_mutex_t node_mutex;
 	struct node *next;
 };
 
 struct list {
 	struct node *head;
-	pthread_mutex_t list_mutex;
 	int size;
 };
 
@@ -34,12 +32,19 @@ int main(int argc, char *argv[])
 	/* seed random number generator */
 	srand(time(NULL));
 
+	/*mutex init*/
+	pthread_mutex_init(&write_mutex, NULL);
+	/*mutex init*/
+	pthread_mutex_init(&read_mutex, NULL);
+	/*mutex init*/
+	pthread_mutex_init(&delete_mutex, NULL);
+
 	/* Philosopher threads */
-	pthread_t readt, readt2, writet, writet2, delt, delt2;
+	pthread_t readt, readt2, writet, writet2, writet3, writet4, delt, delt2;
 
 	/* pthread arrays to simplify initialization */
 	pthread_t* read_threads[2] = {&readt, &readt2};
-	pthread_t* write_threads[2] = {&writet, &writet2};
+	pthread_t* write_threads[4] = {&writet, &writet2};
 	pthread_t* delete_threads[2] = {&delt, &delt2};
 
 	struct list *l_list = (struct list *)malloc(sizeof(struct list));
@@ -47,7 +52,7 @@ int main(int argc, char *argv[])
 
 	int err_check = 0;
 
-	/* Loop through thread array, spawn philosopher threads */
+	/* Loop through thread array, spawn threads */
 	for(int i = 0; i < 2; i++) {
 		err_check = pthread_create(write_threads[i], NULL, write,
 			       (void*)l_list);
@@ -58,8 +63,6 @@ int main(int argc, char *argv[])
 			exit(err_check);
 		}
 	}
-
-
 	for(int i = 0; i < 2; i++) {
 		err_check = pthread_create(read_threads[i], NULL, read,
 			       (void*)l_list);
@@ -70,16 +73,16 @@ int main(int argc, char *argv[])
 			exit(err_check);
 		}
 	}
-	for(int i = 0; i < 2; i++) {
-		err_check = pthread_create(delete_threads[i], NULL, del,
-						 (void*)l_list);
+	for(int i = 0; i < 1; i++) {
+	 	err_check = pthread_create(delete_threads[i], NULL, del,
+	 					 (void*)l_list);
 
-		if(err_check) {
-			fprintf(stderr, "Error, a thread failed to create -"
-								"exit: %i\n", err_check);
-			exit(err_check);
-		}
-	}
+	 	if(err_check) {
+	 		fprintf(stderr, "Error, a thread failed to create -"
+	 							"exit: %i\n", err_check);
+	 		exit(err_check);
+	 	}
+	 }
 	/* Join threads */
 	pthread_join(delt, NULL);
 	pthread_join(delt2, NULL);
@@ -101,27 +104,30 @@ struct node* create_node() {
 }
 
 void delete_node(int index, struct list *l_list) {
-	struct node *rem_node;
+	struct node *rem_node, *prev_node;
 	rem_node = l_list->head;
 
 	if(index == 0) {
 		//Move the list head forward
-		l_list->head = rem_node->next;
+		if(rem_node->next != NULL) {
+			l_list->head = rem_node->next;
+		} else {
+			l_list->head = NULL;
+		}
 	} else {
 		int i = 1;
-		struct node *prev_node;
-		while(i != index) {
+		do {
 			prev_node = rem_node;
 			rem_node = rem_node->next;
 			i++;
-		}
+
+		} while(i != index);
 		//fix the list
 		if(rem_node->next == NULL) {
 			prev_node->next = NULL;
 		} else {
 			prev_node->next = rem_node->next;
 		}
-
 	}
 	//free the node
 	free(rem_node);
@@ -141,7 +147,7 @@ void* read(void *ptr) {
 			}
 			printf(" \n");
 
-			sleep(3);
+			sleep(1);
 		}
 	}
 }
@@ -149,11 +155,13 @@ void* write(void *ptr) {
 	struct list *l_list = (struct list*)ptr;
 	printf("write thread!\n");
 	while(1) {
+		pthread_mutex_lock(&delete_mutex);
 
-		printf("Write Start\n");
 		struct node *new_node = create_node();
 		if(l_list->size == 0) {
+			pthread_mutex_lock(&write_mutex);
 			l_list->head = new_node;
+			pthread_mutex_unlock(&write_mutex);
 		} else {
 			struct node *cur_node = l_list->head;
 			while(cur_node->next != NULL) {
@@ -165,7 +173,7 @@ void* write(void *ptr) {
 			printf("Added a node with value %i\n", cur_node->data);
 		}
 		l_list->size++;
-
+		pthread_mutex_unlock(&delete_mutex);
 		sleep(2);
 	}
 }
@@ -174,14 +182,19 @@ void* del(void *ptr) {
 	struct list *l_list = (struct list*)ptr;
 	printf("delete thread!\n");
 	while(1) {
-		if(l_list->size != 0) {
-			pthread_mutex_lock(&l_list->list_mutex);
+		/*Handles the zero index problem*/
+		if(l_list->size > 1) {
+			pthread_mutex_lock(&delete_mutex);
+			pthread_mutex_lock(&read_mutex);
+			pthread_mutex_lock(&write_mutex);
 			int del_index = rand() % l_list->size;
 			printf("deleting node %i\n", del_index);
 			delete_node(del_index, l_list);
 			l_list->size--;
-			pthread_mutex_unlock(&l_list->list_mutex);
+			pthread_mutex_unlock(&delete_mutex);
+			pthread_mutex_unlock(&read_mutex);
+			pthread_mutex_unlock(&write_mutex);
 		}
-		sleep(5);
+		sleep(3);
 	}
 }
