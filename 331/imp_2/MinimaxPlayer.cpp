09ad56f34bd6fa99238c30940405f1dc04e8fb54
codymalick <cody.malick@gmail.com>
2016-05-03 14:04:05 -0700

    seg fault fix

--- 331/imp_2/MinimaxPlayer.cpp
+++ 331/imp_2/MinimaxPlayer.cpp
@@ -28,36 +28,35 @@ void print_vec(tuple<int,int> moves) {
 	printf("(%i,%i)\n", std::get<0>(moves), std::get<1>(moves));
 }
 
-p_moves* find_possible_moves(OthelloBoard* b, char symbol) {
-	p_moves *moves = (p_moves *)malloc(sizeof(p_moves));
+p_moves find_possible_moves(OthelloBoard* b, char symbol) {
+	//p_moves *moves = (p_moves *)malloc(sizeof(p_moves));
+    p_moves moves;
 	// Collect possible moves
 	int index = 0;
 	for(int i = 0; i < 4; i++) {
 		for(int j = 0; j < 4; j++) {
 			if(b->is_legal_move(i,j,symbol)) {
-			moves->push_back(tuple<int,int>(i,j));
+			    moves.push_back(tuple<int,int>(i,j));
 			}
 		}
 	}
 	std::cout << "wohoo" << std::endl;
 	//debug
-	std::for_each(moves->begin(), moves->end(), print_vec);
+	std::for_each(moves.begin(), moves.end(), print_vec);
 	return moves;
 }
 
 void MinimaxPlayer::get_move(OthelloBoard* b, int& col, int& row) {
 		char symbol = b->get_p2_symbol();
-		p_moves *moves = find_possible_moves(b, symbol);
-
-
+		p_moves moves = find_possible_moves(b, symbol);
 
 		// For each possible move, calculate the utility
 
 		// Set the move value, no return
-		tuple<int,int> selection = moves->front();
+		tuple<int,int> selection = moves.front();
 		col = get<0>(selection);
 		row = get<1>(selection);
-		free(moves);
+		//free(moves);
 }
 
 MinimaxPlayer* MinimaxPlayer::clone() {
