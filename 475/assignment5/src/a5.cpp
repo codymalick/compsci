1d368801b23114d292436c4af883e28959b745d4
codymalick <cody.malick@gmail.com>
2016-05-16 13:29:42 -0700

    project 5 files

---
+++ 475/assignment5/src/a5.cpp
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<iostream>
+#include<omp.h>
+#include<unistd.h>
+#include"simd.p5.h"
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#define NUMT 4
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
