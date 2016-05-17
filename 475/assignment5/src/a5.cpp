6a207af22be7268a5c9230f392431a4ed2678017
codymalick <cody.malick@gmail.com>
2016-05-16 21:32:43 -0700

    finished assignment 5

--- 475/assignment5/src/a5.cpp
+++ 475/assignment5/src/a5.cpp
@@ -18,47 +18,77 @@
 
 #endif //simd_h
 
+#define ONEM 1000000
+
 void SimdMul(float *, float*, float*, int);
 float SimdMulSum(float *, float *, int);
 
 void man_mult(float *, float *, float *, int);
-void man_sum(float *, float *, float *, int);
+void man_sum(float *, float *, int);
 int main(int argc, char *argv[])
 {
 	int iterations = 100;
-	float *array1;
-	float *array2;
-	float *array3;
-	double time0;
-	double time1;
-	double simd_time;
-	double manual_time;
+	float *array1, *array2, *array3;
+	double time0, time1, simd_time, manual_time, simd_both, man_both;
+	double average_simd = 0, top_simd = 100, average_man = 0, top_man = 100;
+	double mults, reducs, max_mults = 0, max_reducs = 0;
+	printf("Size,SIMD Mult, Manual Mult, SIMD Mult/Reduction, Manual Mult/Reduction\n");
 
-	for(int i = 1000; i < 3200000; i += 1000) {
-		array1 = new float[i];
-		array2 = new float[i];
-		array3 = new float[i];
+	for(int j = 0; j < 10; j++) {
+		for(int i = 1000; i <= 3200000; i += 1000) {
+			array1 = new float[i];
+			array2 = new float[i];
+			array3 = new float[i];
 
-		time0 = omp_get_wtime();
+			time0 = omp_get_wtime();
 				
-		// Call simd
-		SimdMul(array1, array2, array3, i);
-		time1 = omp_get_wtime();
-		simd_time = time1 - time0;
-		
-		time0 = omp_get_wtime();
+			// Call simd
+			SimdMul(array1, array2, array3, i);
+			time1 = omp_get_wtime();
+			simd_time = time1 - time0;
 
-		// Call manual mult
-		man_mult(array1, array2, array3, i);
-		time1 = omp_get_wtime();
-		manual_time = time1 - time0;
+			time0 = omp_get_wtime();
 
-		printf("%i,%f,%f\n", i, simd_time, manual_time);
-		// Free arrays to prevent memory overload
-		delete array1;
-		delete array2;
-		delete array3;
+			// Call manual mult
+			man_mult(array1, array2, array3, i);
+			time1 = omp_get_wtime();
+			manual_time = time1 - time0;
+
+			// simd both
+			time0 = omp_get_wtime();
+			SimdMulSum(array1, array2, i);
+			time1 = omp_get_wtime();
+			simd_both = time1 - time0;
+
+			// man both
+			time0 = omp_get_wtime();
+			man_sum(array1, array2, i);
+			time1 = omp_get_wtime();
+			man_both = time1 - time0;
+
+			mults = manual_time/simd_time;
+			if(mults > max_mults)
+				max_mults = mults;
+			
+			reducs = man_both/simd_both;
+			if(reducs > max_reducs)
+				max_reducs = reducs;
+
+			printf("%i,%f,%f\n",i,mults,reducs);
+//			printf("%i,%f,%f,%f,%f,\n",i,i/simd_time/ONEM,
+//					i/manual_time/ONEM,
+//					i/simd_both/ONEM,
+//					i/man_both/ONEM);
+			// Free arrays to prevent memory overload
+			delete array1;
+			delete array2;
+			delete array3;
+		}
 	}
+	// average_values
+	printf("Max Mult Speedup: %f, Max Reduc Speedup: %f\n", max_mults, max_reducs);
+
+	
 
 	return 0;
 }
@@ -69,7 +99,7 @@ void man_mult(float *a, float *b, float *c, int len) {
 	}
 }
 
-void man_sum(float *a, float *b, float *c, int len) {
+void man_sum(float *a, float *b, int len) {
 	float sum = 0;
 	for(int i = 0; i < len; i++) {
 		sum += a[i] * b[i];
